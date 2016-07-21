5da59eb613ac34d092e48b27b5245fa9e6130e2c
Roy <rechase84@gmail.com>
2016-07-21 13:11:20 -0600

    initial commit

---
+++ src/PIN.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function getPIN(min, max) {
+	var PIN = "";
+	var digit;
+ for(var i=0; i<4; i++){
+ 	 digit = Math.trunc(Math.random() * (max - min) + min);
+ 	 PIN += digit;
+ }
+ return PIN;
+}
+
+getPIN(0,9);
